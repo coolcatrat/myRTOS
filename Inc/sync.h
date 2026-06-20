b91d016e1254bc22b10eed80ec1ab534728b9350
coolcatrat <devvratv5@gmail.com>
2026-06-21 02:08:46 +0400

    Added mutex_t locks, with helper functions
     - mutex_init(), mutex_lock(), mutex_unlock() functions added
     - mutex object uses same functions as semaphore - highest_priority_waiter()
        and same tcb field blocked_on.

--- Inc/sync.h
+++ Inc/sync.h
@@ -10,3 +10,12 @@ typedef struct {
 void sem_take(semaphore_t* sem);
 void sem_give(semaphore_t* sem);
 void sem_init(semaphore_t* sem, uint32_t initial, uint32_t maxCount);
+
+
+typedef struct {    
+    tcb_t *owner;           // NULL when free, not NULL when occupied.
+} mutex_t;
+
+void mutex_init(mutex_t *m);
+void mutex_lock(mutex_t *m);
+void mutex_unlock(mutex_t *m);
--- Src/sync.c
+++ Src/sync.c
@@ -41,3 +41,50 @@ void sem_init(semaphore_t* sem, uint32_t initial, uint32_t maxCount){
     sem->count = initial;
     sem->max_count = maxCount;
 }
+
+void mutex_init(mutex_t *m){
+    m->owner = NULL;
+}
+void mutex_lock(mutex_t *m){
+    uint32_t sr = os_enter_critical();
+
+    // if unlocked, assign current task as the owner and lock it.
+    if(!m->owner){
+        m->owner = current_tcb;
+        os_exit_critical(sr);
+        return;
+    }
+    // if locked, declare task waiting on mutex_t m and block it. 
+    current_tcb->blocked_on = m;
+    current_tcb->state = TASK_BLOCKED;
+    os_yield_to_scheduler();        // request context switch
+    os_exit_critical(sr);           
+}
+void mutex_unlock(mutex_t *m){
+    uint32_t sr = os_enter_critical();
+
+    // the task calling is not the owner, should not happen generally.
+    if(current_tcb != m->owner){
+        os_exit_critical(sr);
+        return;
+    }
+
+    // unblock the next highest priority task waiting on the mutex
+    tcb_t *waiter = highest_priority_waiter(m);
+
+    // if a waiter exists, it is the next owner of the mutex.
+    if(waiter != NULL){
+        m->owner = waiter;
+        waiter->blocked_on = NULL;
+        waiter->state = TASK_READY;
+        scheduler_ready_add(waiter);
+        os_yield_to_scheduler();
+        os_exit_critical(sr);
+    }
+    // if no waiter exists, mutex is free.
+    else{
+        m->owner = NULL;
+        os_exit_critical(sr);
+    }
+
+}
